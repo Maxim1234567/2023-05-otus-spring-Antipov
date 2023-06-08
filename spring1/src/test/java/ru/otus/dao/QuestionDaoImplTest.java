e360cb2a20206af88c0893d87a07b26b484cd10f
maksim_antipov_1 <anti123456.antipov@yandex.ru>
2023-06-08 18:51:43 +0300

    поправил замечания

--- spring1/src/test/java/ru/otus/dao/QuestionDaoImplTest.java
+++ spring1/src/test/java/ru/otus/dao/QuestionDaoImplTest.java
@@ -3,7 +3,6 @@ package ru.otus.dao;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import ru.otus.domain.Answer;
 import ru.otus.domain.Question;
@@ -11,32 +10,33 @@ import ru.otus.domain.Question;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
 public class QuestionDaoImplTest {
-    private final int BIG_INDEX = Integer.MAX_VALUE;
-
-    @Mock
-    private CsvDataSource csvDataSource;
-    private QuestionDao questionDao;
-
-    private Question question;
+     private QuestionDao questionDao;
     private List<Question> questions;
 
     @BeforeEach
     void setUp() {
-        questionDao = new QuestionDaoImpl(csvDataSource);
-        question = new Question("Question", List.of(new Answer("Answer")));
-        questions = List.of(new Question("Question1", List.of(new Answer("Answer1"))));
+        questionDao = new QuestionDaoImpl("/data.csv", ";");
+        questions = List.of(
+                new Question("Test Answer 1?", List.of(
+                        new Answer("1"),
+                        new Answer("1"),
+                        new Answer("1"))),
+                new Question("Test Answer 2?", List.of(
+                        new Answer("2"),
+                        new Answer("2"))),
+                new Question("Test Answer 3?", List.of(
+                        new Answer("3")))
+        );
     }
 
     @Test
-    void correctReturnAllQuestion() {
-        given(csvDataSource.getAllQuestions())
-                .willReturn(questions);
-
-        assertThat(questionDao.getAllQuestions())
-                .isEqualTo(questions);
+    void correctParseFileCsv() {
+        List<Question> result = questionDao.getAllQuestions();
+        assertEquals(result, questions);
     }
 }
